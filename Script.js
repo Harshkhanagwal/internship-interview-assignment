0b597c5946abf4c4868c4bc76e5f58f13521cc18
harsh khanagwal <harshkhanagwall29@gmail.com>
2024-07-12 14:50:31 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,48 @@
+const pcard1 = document.getElementById("pcard1");
+const pcard2 = document.getElementById("pcard2");
+const pcard3 = document.getElementById("pcard3");
+const projectimg = document.getElementById("projectimg");
+
+pcard2.classList.toggle("red-card");
+
+
+const setProduct = (card) => {
+    if (card === 1) {
+        pcard1.classList.add("red-card");
+        pcard2.classList.remove("red-card");
+        pcard3.classList.remove("red-card");
+
+        projectimg.src='./Images/project2.jpg'
+    }else if(card===2){
+        pcard1.classList.remove("red-card");
+        pcard2.classList.add("red-card");
+        pcard3.classList.remove("red-card");
+        projectimg.src='./Images/product.png'
+
+    }else{
+        pcard1.classList.remove("red-card");
+        pcard2.classList.remove("red-card");
+        pcard3.classList.add("red-card");
+        projectimg.src='./Images/project3.jpg'
+
+    }
+}
+
+const radios = document.querySelectorAll('input[name="position"]');
+
+
+function handleRadioChange(event) {
+    // Get the value of the selected radio button
+    const positionValue = event.target.value;
+
+    // Get the target element
+    const targetElement = document.getElementById('slider__inner');
+
+    // Set the left position in percentage based on the selected radio button
+    targetElement.style.left = `${positionValue}%`;
+}
+
+// Attach the change event listener to each radio button
+radios.forEach(radio => {
+    radio.addEventListener('change', handleRadioChange);
+});
